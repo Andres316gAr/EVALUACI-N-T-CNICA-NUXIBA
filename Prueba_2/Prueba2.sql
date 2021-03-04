56f8a7df38a528493b06e0aadc7c1bc9e5c49407
andres316gar <lagg.andres@gmail.com>
2021-03-04 12:28:33 -0600

    Evaluacion Tecnica
    Evaluacion Tecnica

--- Prueba_2/Prueba2.sql
+++ Prueba_2/Prueba2.sql
@@ -28,7 +28,7 @@ select * from [dbo].[logDial] where tipoDeLlamada = 'Cel LD' and fechaDeLlamada 
 -- 2-Que indique el promedio de tiempo de dialogo de las llamadas con tipo **Cel LD** durante el **mes de febrero** **_(5 puntos)_**
 declare @inicio date ='2020-01-31';
 declare @fin date = '2020-03-01';
-select AVG(tiempoDialogo) from [dbo].[logDial] where  tipoDeLlamada 'Cel LD' and fechaDeLlamada >@inicio and fechaDeLlamada < DATEADD(day, 1, @fin)
+select AVG(tiempoDialogo) from [dbo].[logDial] where  tipoDeLlamada = 'Cel LD' and fechaDeLlamada >@inicio and fechaDeLlamada < DATEADD(day, 1, @fin)
 
 -- 3-Que muestre el **número en minutos de dialogo** (tomando tiempoDialogo que está en segundos) y el **costo** de todas las llamadas del **mes de enero** **_(10 puntos)_**
 
